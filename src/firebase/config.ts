4e83fb593e1a19dfdf565e85328443b1daedfc3a
mahmoud ezz <ezz@entrestate.com>
2025-12-02 19:43:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7730943652-a28e0",
+  "appId": "1:622785056226:web:36f7c27873e950997a7dbb",
+  "apiKey": "AIzaSyByDVWTUBxg6XAYaq0l1aIvDqsvc4CU8YI",
+  "authDomain": "studio-7730943652-a28e0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "622785056226"
+};
